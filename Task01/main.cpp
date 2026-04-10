dffd3f0e87deb962399707a835c4f1a18c0d43b7
Tsimofey Vertsinskiy <timoxa2011gold@gmail.com>
2026-04-10 18:33:53 +0300

    upadte logic and main

--- Task01/main.cpp
+++ Task01/main.cpp
@@ -1,4 +1,5 @@
 #include "util.h"
+#include "logic.h"
 
 int main() {
 	int array[DEFAULT_SIZE];
@@ -8,10 +9,17 @@ int main() {
 
 	cout << "Input size of array: ";
 	cin >> size;
-	
+
 	init(array, size, 0, 100);
 
-	print ("Array: ");
+	print("Array: ");
 	print(convert(array, size));
+
+	int max = get_max(array, size);
+	int min = get_min(array, size);
+
+	print("\nMax value is " + to_string(max) + ".");
+	print("\nMin value is " + to_string(min) + ".");
+
 	return 0;
 }
