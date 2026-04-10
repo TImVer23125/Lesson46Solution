a685a04ee56c3708992e14acec63bdfdd92b2b7c
Tsimofey Vertsinskiy <timoxa2011gold@gmail.com>
2026-04-10 18:14:51 +0300

    Update util,main

--- Task01/main.cpp
+++ Task01/main.cpp
@@ -1,8 +1,17 @@
-#include "logic.h"
+#include "util.h"
 
 int main() {
+	int array[DEFAULT_SIZE];
+	int a, b;
 
+	int size;
 
+	cout << "Input size of array: ";
+	cin >> size;
+	
+	init(array, size, 0, 100);
 
+	print ("Array: ");
+	print(convert(array, size));
 	return 0;
 }
